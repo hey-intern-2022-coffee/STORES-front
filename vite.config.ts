6aabe2179a02a750808b67702173e9d4f0179c90
Jabelic <hibiki990523@gmail.com>
2022-09-13 12:42:06 +0900

    FIX: for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src').replace(/\\/g, '/')
     }
   },
-  plugins: [vue(), manualChunksPlugin()],
+  plugins: [
+    vue()
+    // , manualChunksPlugin()
+  ],
   test: {
     globals: true,
     environment: 'happy-dom'
