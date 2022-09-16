4b475f0d64ea2e2e9896bbfad31a65efe35def23
Jabelic <hibiki990523@gmail.com>
2022-09-16 10:14:27 +0900

    types

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import piniaPersist from 'pinia-plugin-persist'
+// import piniaPersist from 'pinia-plugin-persist'
+import { createPersistedStatePlugin } from 'pinia-plugin-persistedstate-2'
 
 const pinia = createPinia()
-pinia.use(piniaPersist)
+const installPersistedStatePlugin = createPersistedStatePlugin()
+pinia.use(context => installPersistedStatePlugin(context))
+// pinia.use(piniaPersist)
 
 const app = createApp(App)
 app.use(pinia)
